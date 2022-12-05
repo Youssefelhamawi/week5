d567af16c02ba583eff1c3ca0f4150ff43d41882
Youssefelhamawi <117123236+Youssefelhamawi@users.noreply.github.com>
2022-12-05 10:09:11 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementById("button").addEventListener("click",info)
+function info(){
+
+
+fetch("https://youssefelhamawi.github.io/week5/college.json")
+
+.then((response)=>{
+   return response.json();
+})
+.then((items)=>{
+   let placeholder = document.querySelector("#data-output");
+   let tabledata=document.querySelector("#tabledata");
+   let tableinfo = `
+    <tr>
+        <th>Image</th>
+        <th>University</th>
+        <th>Major</th>
+        <th>Degree</th>
+        <th>Year Graduated</th>
+    </tr>`
+   let out = "";
+   for(let item of items){
+      out += `
+         <tr>
+            <td> <img src='${item.image}'> </td>
+            <td>${item.name}</td>
+            <td>${item.program}</td>
+            <td>${item.type}</td>
+            <td>${item.year}</td>
+         </tr>
+      `;
+   }
+ 
+   placeholder.innerHTML = out;
+   tabledata.innerHTML = tableinfo;
+})
+.catch(() => {
+    console.log("json file is not found");
+});
+};
